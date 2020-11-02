82720b6a3ad364e3e90bcdb128bf64b6654211a2
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-11-02 14:23:02 -0500

    set up initial form inputs and button

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,46 @@ import React from 'react';
 export default function Form(){
     return(
         <div className='form'>
-            Form will go here
+            {/* Name */}
+            <label htmlFor='name'>Name</label>
+            <input 
+            name = 'name'
+            type='text'
+            id = 'name'
+            placeholder = 'name'
+            // value = ''
+            required
+            />
+            {/* Email */}
+            <label htmlFor='email'>Email</label>
+            <input 
+            name = 'email'
+            type='email'
+            id = 'email'
+            placeholder = 'email'
+            // value = ''
+            required
+            />
+            {/* Password */}
+            <label htmlFor='password'>Password</label>
+            <input 
+            name = 'password'
+            type='password'
+            id = 'password'
+            placeholder = 'password'
+            // value = ''
+            required
+            />
+            {/* Terms of Service Checkbox */}
+            <label for="terms">Terms</label>
+            <input 
+            name='terms'
+            type='checkbox' 
+            id='terms' 
+            checked
+            />
+            {/* Submit Button */}
+            <button disabled={false} type='submit'>Submit</button>
         </div>
     )
 }
