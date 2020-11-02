796f53806d3dc67ef156dc8587e07d3f8b7b5c8e
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-11-02 14:08:00 -0500

    created new react app, added yup and axios dependencies, created components folder and form component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Form from './components/Form'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Onboarding</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
