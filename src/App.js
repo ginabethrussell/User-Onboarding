bac20822021af7f2561bbfd15c79394a3ad6d65b
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-11-03 12:06:26 -0500

    added a component to display users, completed stretch tasks for styling, addind addtional inputs, and checking for disallowed email

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import Form from './components/Form'
+import React, {useState} from 'react';
+import Form from './components/Form';
+import TeamMembers from './components/TeamMembers';
 import './App.css';
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState([]);
+
+  const updateTeam = (users) => {
+    // const newTeamList = [...users];
+    console.log('update team for display')
+    setTeamMembers(users);
+  }
+
   return (
-    <div className="App">
-      <h1>User Onboarding</h1>
-      <Form />
+    <div className="App container">
+      <img className='schoolLogo' src="https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5dcda59e63bb6ae5c9282801_small-red-logo.png" alt="logo"/>
+      <h1>Build Week Teams</h1>
+      <h2>Sign Up</h2>
+      <Form updateTeam={updateTeam}/>
+      <TeamMembers team={teamMembers} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -7,10 +7,13 @@ const initialFormState = {
     email: '',
     password: '',
     role: '',
+    product1: '',
+    product2: '',
+    product3: '',
     terms: true
 }
 
-export default function Form(){
+export default function Form(props){
     const [formState, setFormState] = useState(initialFormState);
     const [buttonDisabled, setButtonDisabled] = useState(true);
     const [errors, setErrors] = useState({
@@ -18,15 +21,22 @@ export default function Form(){
         email: '',
         password: '',
         role: '',
+        product1: '',
+        product2: '',
+        product3: '',
         terms: ''
         });
     const [users, setUsers] = useState([]);
+    const {updateTeam} = props;
 
     const formSchema = yup.object().shape({
         name: yup.string().matches(/[a-zA-Z\s]/, 'Name is required and must only contain letters and spaces.'),
-        email: yup.string().email('Please enter a valid email.'),
+        email: yup.string().email('Please enter a valid email.').notOneOf(['waffles@syrup.com'], 'That email is already taken.'),
         password: yup.string().min(6,'Password must contain 6 characters'),
         role: yup.string().min(1,'Please select a role.'),
+        product1: yup.string(),
+        product2: yup.string(),
+        product3: yup.string(),
         terms: yup.bool().oneOf([true],"You must agree to the terms and conditions.")
     });
 
@@ -64,17 +74,23 @@ export default function Form(){
         });
       }, [formState]);
 
+    useEffect(() => {
+        updateTeam(users);
+        console.log('....updating team');
+    }, [users]);
+
     const submitForm = (e) => {
         e.preventDefault();
         console.log(formState);
-        setFormState(initialFormState);
         axios.post( 'https://reqres.in/api/users', formState)
         .then(response => setUsers([...users, response.data]))
         .catch(err => console.log(err))
+        setFormState(initialFormState);
     }
 
     return(
         <form onSubmit={submitForm} className='form'>
+            <div className="form-group">
             {/* Name */}
             <label htmlFor='name'>Name</label>
             <input 
@@ -87,6 +103,8 @@ export default function Form(){
             onChange={onInputChange}
             />
             {errors.name.length > 0? (<p className="errors">{errors.name}</p>): null}
+            </div>
+            <div className="form-group">
             {/* Email */}
             <label htmlFor='email'>Email</label>
             <input 
@@ -99,6 +117,8 @@ export default function Form(){
             onChange={onInputChange}
             />
              {errors.email.length > 0? (<p className="errors">{errors.email}</p>): null}
+             </div>
+             <div className="form-group">
             {/* Password */}
             <label htmlFor='password'>Password</label>
             <input 
@@ -111,17 +131,84 @@ export default function Form(){
             onChange={onInputChange}
             />
             {errors.password.length > 0? (<p className="errors">{errors.password}</p>): null}
+            </div>
+            <div className='form-group'>
             {/* Role */}
             <label htmlFor='role'>Role</label>
-            <select id='role' name='role' required  onChange={onInputChange}>
+            <select id='role' name='role' required onChange={onInputChange}>
                 <option value=''>--select your role--</option>
-                <option value='admin'>Admin</option>
-                <option value='volunteer'>Volunteer</option>
-                <option value='student'>Student</option>
+                <option value='UI'>UI Designer</option>
+                <option value='Front End I'>Front End Developer I</option>
+                <option value='Front End II'>Front End Developer II</option>
+                <option value='Back End'>Back End Developer</option>
+                <option value='PL'>Project Lead</option>
             </select>
             {errors.role.length > 0? (<p className="errors">{errors.role}</p>): null}
+            </div>
+            <h3>Marketing Products Preference</h3>
+            <div className='form-group'>
+            {/* Product Choice 1 */}
+            <label htmlFor='product1'>Product 1st Choice</label>
+            <select id='product1' name='product1' onChange={onInputChange}>
+                <option value=''>--select your first choice--</option>
+                <option value='African Marketplace'>African Marketplace</option>
+                <option value='Anywhere Fitness'>Anywhere Fitness</option>
+                <option value='Better Professor App'>Better Professor App</option>
+                <option value='Co-Make'>Co-Make</option>
+                <option value='DevDesk Queue'>DevDesk Queue</option>
+                <option value='How-To'>How-To</option>
+                <option value='Pintereach'>Pintereach</option>
+                <option value='Expat Journal'>Expat Journal</option>
+                <option value='FoodTruck TrackR'>FoodTruck TrackR</option>
+                <option value='Potluck Planner'>Pot Luck Planner</option>
+                <option value='School in the Cloud'>School in the Cloud</option>
+                <option value='Secret Family Recipes'>Secret Family Recipes</option>
+            </select>
+            {errors.product1.length > 0? (<p className="errors">{errors.product1}</p>): null}
+            </div>
+            <div className='form-group'>
+            {/* Product Choice 2 */}
+            <label htmlFor='product12'>Product 2nd Choice</label>
+            <select id='product2' name='product2' onChange={onInputChange}>
+            <option value=''>--select your second choice--</option>
+                <option value='African Marketplace'>African Marketplace</option>
+                <option value='Anywhere Fitness'>Anywhere Fitness</option>
+                <option value='Better Professor App'>Better Professor App</option>
+                <option value='Co-Make'>Co-Make</option>
+                <option value='DevDesk Queue'>DevDesk Queue</option>
+                <option value='How-To'>How-To</option>
+                <option value='Pintereach'>Pintereach</option>
+                <option value='Expat Journal'>Expat Journal</option>
+                <option value='FoodTruck TrackR'>FoodTruck TrackR</option>
+                <option value='Potluck Planner'>Pot Luck Planner</option>
+                <option value='School in the Cloud'>School in the Cloud</option>
+                <option value='Secret Family Recipes'>Secret Family Recipes</option>
+            </select>
+            {errors.product2.length > 0? (<p className="errors">{errors.product2}</p>): null}
+            </div>
+            <div className='form-group'>
+            {/* Product Choice 3 */}
+            <label htmlFor='product3'>Product 3rd Choice</label>
+            <select id='product3' name='product3' onChange={onInputChange}>
+            <option value=''>--select your third choice--</option>
+                <option value='African Marketplace'>African Marketplace</option>
+                <option value='Anywhere Fitness'>Anywhere Fitness</option>
+                <option value='Better Professor App'>Better Professor App</option>
+                <option value='Co-Make'>Co-Make</option>
+                <option value='DevDesk Queue'>DevDesk Queue</option>
+                <option value='How-To'>How-To</option>
+                <option value='Pintereach'>Pintereach</option>
+                <option value='Expat Journal'>Expat Journal</option>
+                <option value='FoodTruck TrackR'>FoodTruck TrackR</option>
+                <option value='Potluck Planner'>Pot Luck Planner</option>
+                <option value='School in the Cloud'>School in the Cloud</option>
+                <option value='Secret Family Recipes'>Secret Family Recipes</option>
+            </select>
+            {errors.product3.length > 0? (<p className="errors">{errors.product3}</p>): null}
+            </div>
+            <div className="form-group checkbox-group">
             {/* Terms of Service Checkbox */}
-            <label for="terms">Terms</label>
+            <label htmlFor="terms">Terms</label>
             <input 
             name='terms'
             type='checkbox' 
@@ -130,9 +217,10 @@ export default function Form(){
             onChange={onInputChange}
             />
             {errors.terms.length > 0? (<p className="errors">{errors.terms}</p>): null}
+            </div>
             {/* Submit Button */}
             <button disabled={buttonDisabled} type='submit'>Submit</button>
-            <pre width='300px'>{JSON.stringify(users, null, 2)}</pre>
+            {/* <pre width='300px'>{JSON.stringify(users, null, 2)}</pre> */}
         </form>
     )
 }
